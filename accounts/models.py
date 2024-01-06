744b4e2b9b11201e574d6df5b5597dd7dccb02bf
DarshanMady <maddidarshan2501@gmail.com>
2024-01-06 13:49:17 +0530

    email verification p1

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
 class MyAccountManager(BaseUserManager):
-    def create_user(self,first_name,last_name,username,email,password=None):
+    def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
             raise ValueError('Please provide an email address')
         if not username:
@@ -41,8 +41,8 @@ class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50,unique=True)
-    email = models.EmailField(max_length=50,unique=True)
-    phone_number = models.CharField(max_length=50,unique=True)
+    email = models.EmailField(max_length=100,unique=True)
+    phone_number = models.CharField(max_length=10, blank=False, null=False)
 
     # Required fields for creating superuser or admin
 
